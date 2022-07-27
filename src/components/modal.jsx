393d2063230efa519c99caae88afa4abccee51fb
Mark Stevens <markspstevens@yahoo.co.uk>
2022-07-27 16:13:21 +0200

    readme update

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useContext } from 'react';
 import { Context } from '../context/globalProvider';
 import { createPortal } from 'react-dom';
-// import components/functions...
+// import components/functions/libraries...
+import FocusTrap from 'focus-trap-react';
 import Animate from '../utils/Animate';
 import AddRemoveButton from './buttons/AddRemoveButton';
 import { IMAGE_URL } from '../config/requests';
@@ -28,45 +29,7 @@ const Modal = () => {
 		const modal = document.getElementById('modal');
 		body.setAttribute('aria-hidden', modalIsOpen ? 'true' : 'false');
 		modal.setAttribute('aria-hidden', modalIsOpen ? 'false' : 'true');
-		// initiate keyboard listener to focus trap modal
-		modalIsOpen && document.addEventListener('keydown', handleKeydown);
-
-		return () => {
-			// Detach listener when component unmounts
-			document.removeEventListener('keydown', handleKeydown);
-		};
 	});
-
-	// Focus Trap
-	const modalRef = React.createRef();
-
-	const handleTabKey = (e) => {
-		const focusableModalElements = modalRef.current.querySelectorAll(
-			'a[href], button, textarea, input[type="text"], input[type="radio"], input[type="checkbox"], select'
-		);
-		const firstElement = focusableModalElements[0];
-		const lastElement =
-			focusableModalElements[focusableModalElements.length - 1];
-		if (!e.shiftKey && document.activeElement !== firstElement) {
-			firstElement.focus();
-			return e.preventDefault();
-		}
-
-		if (e.shiftKey && document.activeElement !== lastElement) {
-			lastElement.focus();
-			e.preventDefault();
-		}
-	};
-
-	const handleKeydown = (e) => {
-		const listener = keyListenersMap.get(e.keyCode); // get the listener corresponding to the pressed key
-		return listener && listener(e); // call the listener if it exists
-	};
-
-	const keyListenersMap = new Map([
-		[27, closeModal],
-		[9, handleTabKey],
-	]);
 
 	return createPortal(
 		<div
@@ -75,7 +38,6 @@ const Modal = () => {
 			aria-modal="true"
 			aria-labelledby="modal__title"
 			className="modal"
-			ref={modalRef}
 			style={{
 				animation: !modalIsOpen
 					? 'modalBgFadeOut 0.1s ease-in-out both 0.4s'
@@ -88,102 +50,104 @@ const Modal = () => {
 				animateIn={'modalopen'}
 				animateOut={'modalclose'}
 			>
-				<div className="modal__body">
-					{media && (
-						<>
-							<header>
-								<button
-									aria-label="Close"
-									className="modal__closeButton"
-									onClick={() => closeModal()}
-								>
-									<img src={closeButton} alt="close modal" />
-								</button>
-
-								<img
-									className="modal__image"
-									src={
-										media.backdrop_path !== null
-											? `${IMAGE_URL}${media.backdrop_path}`
-											: media.poster_path !== null
-											? `${IMAGE_URL}${media.poster_path}`
-											: noImage
-									}
-									alt={media?.title || media?.name || media?.original_title}
-								/>
-
-								<span className="modal__buttons">
+				<FocusTrap active={modalIsOpen}>
+					<div className="modal__body">
+						{media && (
+							<>
+								<header>
 									<button
-										className="button button--playModal"
-										aria-label="Play Video Trailer"
-										onClick={() => handleVideoDetails(media)}
+										aria-label="Close"
+										className="modal__closeButton"
+										onClick={() => closeModal()}
 									>
-										<img src={playIcon} alt="" />
-										Play
+										<img src={closeButton} alt="close modal" />
 									</button>
 
-									<AddRemoveButton
-										media={media}
-										classType={'modal__likeButton'}
+									<img
+										className="modal__image"
+										src={
+											media.backdrop_path !== null
+												? `${IMAGE_URL}${media.backdrop_path}`
+												: media.poster_path !== null
+												? `${IMAGE_URL}${media.poster_path}`
+												: noImage
+										}
+										alt={media?.title || media?.name || media?.original_title}
 									/>
-								</span>
-							</header>
 
-							<section className="modal__content">
-								<h1 id="modal__title">
-									{media?.title || media?.name || media?.original_title}
-								</h1>
-								<p className="modal__details__overview modal__details__animate--1">
-									{media.overview ? media.overview : 'Not available'}
-								</p>
+									<span className="modal__buttons">
+										<button
+											className="button button--playModal"
+											aria-label="Play Video Trailer"
+											onClick={() => handleVideoDetails(media)}
+										>
+											<img src={playIcon} alt="" />
+											Play
+										</button>
 
-								<article>
-									<h2>Details</h2>
-									<div className="modal__details">
-										<span className="modal__details__label modal__details__animate--2">
-											Genres :{' '}
-										</span>
-										<span className="modal__details__text modal__details__animate--2">
-											{media.genre_ids && (
-												<GenresList
-													genreIds={media.genre_ids}
-													classType={'modal'}
-												/>
-											)}
-										</span>
+										<AddRemoveButton
+											media={media}
+											classType={'modal__likeButton'}
+										/>
+									</span>
+								</header>
 
-										<span className="modal__details__label modal__details__animate--3">
-											Original Language :{' '}
-										</span>
-										<span className="modal__details__text modal__details__animate--3">
-											{capitalize(media?.original_language)}
-										</span>
+								<section className="modal__content">
+									<h1 id="modal__title">
+										{media?.title || media?.name || media?.original_title}
+									</h1>
+									<p className="modal__details__overview modal__details__animate--1">
+										{media.overview ? media.overview : 'Not available'}
+									</p>
 
-										<span className="modal__details__label modal__details__animate--4">
-											{media.release_date ? 'Release Date' : 'First Aired'}{' '}
-										</span>
-										<span className="modal__details__text modal__details__animate--4">
-											{media.release_date
-												? ConvertDate(media.release_date)
-												: media.first_air_date
-												? ConvertDate(media.first_air_date)
-												: 'Not Available'}
-										</span>
+									<article>
+										<h2>Details</h2>
+										<div className="modal__details">
+											<span className="modal__details__label modal__details__animate--2">
+												Genres :{' '}
+											</span>
+											<span className="modal__details__text modal__details__animate--2">
+												{media.genre_ids && (
+													<GenresList
+														genreIds={media.genre_ids}
+														classType={'modal'}
+													/>
+												)}
+											</span>
 
-										<span className="modal__details__label modal__details__animate--5">
-											Average Vote :{' '}
-										</span>
-										<span className="modal__details__text modal__details__animate--5">
-											{media.vote_average || media.vote_average === 0
-												? Math.round(media.vote_average)
-												: 'Not Available'}
-										</span>
-									</div>
-								</article>
-							</section>
-						</>
-					)}
-				</div>
+											<span className="modal__details__label modal__details__animate--3">
+												Original Language :{' '}
+											</span>
+											<span className="modal__details__text modal__details__animate--3">
+												{capitalize(media?.original_language)}
+											</span>
+
+											<span className="modal__details__label modal__details__animate--4">
+												{media.release_date ? 'Release Date' : 'First Aired'}{' '}
+											</span>
+											<span className="modal__details__text modal__details__animate--4">
+												{media.release_date
+													? ConvertDate(media.release_date)
+													: media.first_air_date
+													? ConvertDate(media.first_air_date)
+													: 'Not Available'}
+											</span>
+
+											<span className="modal__details__label modal__details__animate--5">
+												Average Vote :{' '}
+											</span>
+											<span className="modal__details__text modal__details__animate--5">
+												{media.vote_average || media.vote_average === 0
+													? Math.round(media.vote_average)
+													: 'Not Available'}
+											</span>
+										</div>
+									</article>
+								</section>
+							</>
+						)}
+					</div>
+				</FocusTrap>
 			</Animate>
 		</div>,
 		document.body
--- src/components/player/YouTubeModal.jsx
+++ src/components/player/YouTubeModal.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useContext } from 'react';
 import { Context } from '../../context/globalProvider';
 import { createPortal } from 'react-dom';
-// import components/functions...
+// import components/functions/libraries...
+import FocusTrap from 'focus-trap-react';
 import YoutubeEmbed from './YouTubeEmbed';
 import Animate from '../../utils/Animate';
 // import images/icons
@@ -18,56 +19,16 @@ const YouTubeModal = () => {
 		useContext(Context);
 	const media = mediaVideoDetails;
 
-	// Focus Trap
-	const modalVideoRef = React.createRef();
-
-	const handleTabKey = (e) => {
-		const focusableModalElements = modalVideoRef.current.querySelectorAll(
-			'a[href], button, textarea, input[type="text"], input[type="radio"], input[type="checkbox"], select'
-		);
-
-		console.log(focusableModalElements)
-		const firstElement = focusableModalElements[0];
-		const lastElement =
-			focusableModalElements[focusableModalElements.length - 1];
-		if (!e.shiftKey && document.activeElement !== firstElement) {
-			firstElement.focus();
-			return e.preventDefault();
-		}
-
-		if (e.shiftKey && document.activeElement !== lastElement) {
-			lastElement.focus();
-			e.preventDefault();
-		}
-	};
-
-	const handleKeydown = (e) => {
-		const listener = keyListenersMap.get(e.keyCode); // get the listener corresponding to the pressed key
-		return listener && listener(e); // call the listener if it exists
-	};
-
-	const keyListenersMap = new Map([
-		[27, closeVideoModal],
-		[9, handleTabKey],
-	]);
-
 	useEffect(() => {
 		// set accessibility for modal open/closed
 		const body = document.querySelector('body');
 		const videoModal = document.getElementById('videoModal');
 		body.setAttribute('aria-hidden', videoModalIsOpen ? 'true' : 'false');
 		videoModal.setAttribute('aria-hidden', videoModalIsOpen ? 'false' : 'true');
-		// initiate keyboard listener to focus trap modal
-		videoModalIsOpen && document.addEventListener('keydown', handleKeydown);
-
-		return () => {
-			document.removeEventListener('keydown', handleKeydown);
-		};
 	});
 
 	return createPortal(
 		<div
-		ref={modalVideoRef}
 			id="videoModal"
 			role="dialog"
 			aria-modal="true"
@@ -85,32 +46,34 @@ const YouTubeModal = () => {
 				animateIn={'modalopen'}
 				animateOut={'modalclose'}
 			>
-				<div className="videoModal__body">
-					{media && (
-						<>
-							<section className="videoModal__content">
-								<h1 className="sr-only" id="videoModal__title">
-									{media?.title || media?.name || media?.original_title}
-								</h1>
-								<button
-									aria-label="Close"
-									className="videoModal__closeButton"
-									onClick={() => closeVideoModal()}
-								>
-									<img src={closeButton} alt="close modal" />
-								</button>
-								{trailerUrl ? (
-									<YoutubeEmbed embedId={trailerUrl} />
-								) : (
-									<div className="videoModal__noImage">
-										<h2 className="sr-only">Sorry, No video available...</h2>
-										<img src={noVideoImage} alt="No video available" />
-									</div>
-								)}
-							</section>
-						</>
-					)}
-				</div>
+				<FocusTrap active={videoModalIsOpen}>
+					<div className="videoModal__body">
+						{media && (
+							<>
+								<section className="videoModal__content">
+									<h1 className="sr-only" id="videoModal__title">
+										{media?.title || media?.name || media?.original_title}
+									</h1>
+									<button
+										aria-label="Close"
+										className="videoModal__closeButton"
+										onClick={() => closeVideoModal()}
+									>
+										<img src={closeButton} alt="close modal" />
+									</button>
+									{trailerUrl ? (
+										<YoutubeEmbed embedId={trailerUrl} />
+									) : (
+										<div className="videoModal__noImage">
+											<h2 className="sr-only">Sorry, No video available...</h2>
+											<img src={noVideoImage} alt="No video available" />
+										</div>
+									)}
+								</section>
+							</>
+						)}
+					</div>
+				</FocusTrap>
 			</Animate>
 		</div>,
 		document.body
